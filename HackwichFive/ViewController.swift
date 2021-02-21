382aa5b56dcd3186e3cb4fea579e734b3a6f316a
KKDR1998 <77356024+KKDR1998@users.noreply.github.com>
2021-02-20 18:59:53 -1000

    Completed Part 8

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -24,6 +24,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var bottomLabel: UILabel!
     
+    //IBOutlet for button to accesss it's properties -- change its attributes
+    
+    @IBOutlet weak var buttonLabel: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,11 +38,44 @@ class ViewController: UIViewController {
         
         //set bottomLabel to display the item at "currentIndex" upon launch of app
         
-        self.bottomLabel.text = favoriteFoodsArray[currentIndex]
+        bottomLabel.text = favoriteFoodsArray[currentIndex]
         
         
     }
 
 
+    @IBAction func buttonPressed(_ sender: Any)
+    {
+        //detects between the index and array numbers, to see which one is higher so the if-block will actiavte when the former is smaller than the latter
+        if self.currentIndex < self.favoriteFoodsArray.count
+        
+        {
+        
+            
+            //set bottomLaveltext to item in the array at currentIndex
+            bottomLabel.text = favoriteFoodsArray[currentIndex]
+            //set the title of the button to "Next"
+            buttonLabel.setTitle("Next", for: UIControl.State.normal)
+            //increases the index number by 1 so the word changes
+            currentIndex += 1
+
+            
+        }else{
+            
+            //prints on consol so I know it works
+            print("Button has been disabled")
+            //disables the button so it cannot be pressed anymore.
+            (buttonLabel!).isEnabled = false
+            
+        }
+    
+    
+    
+    
+    }
+    
+    
+    
+    
 }
 
