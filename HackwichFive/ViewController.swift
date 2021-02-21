91fb3d9ae9dd47ed594182a79ead0206e222e2b7
KKDR1998 <77356024+KKDR1998@users.noreply.github.com>
2021-02-20 18:35:28 -1000

    completed parts 1-7

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // declare and assign a global variable
+    
+    var currentIndex = 0
+    
+    // declare and assign a global variable that is an array that holds a collection of strings that are names of our favorite foods
+    
+    var favoriteFoodsArray = ["Burgers", "Poke", "Loco Moco", "Fries",]
+    
+    
+    
+    @IBOutlet weak var topLabel: UILabel!
+    
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        topLabel.text = "My Favorite Foods"
+        
+        //set bottomLabel to display the item at "currentIndex" upon launch of app
+        
+        self.bottomLabel.text = favoriteFoodsArray[currentIndex]
+        
+        
     }
 
 
